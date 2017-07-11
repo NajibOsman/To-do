43f255cc9022cd4f75bbc8079f30bd55c9144ee2
NajibOsman <machidydemajka38@gmail.com>
2017-07-11 14:57:05 +0300

    ficommit for denno

--- lib/task.rb
+++ lib/task.rb
@@ -1,22 +1,35 @@
 class Task
-    @@all_tasks = []
-    define_method(:initialize) do |description|
-      @description = description
+    attr_reader(:description)
+    
+    define_method(:initialize) do |attributes|
+      @description = attributes.fetch(:description)
     end
 
-    define_method(:description) do
-      @description
-    end
 #class method
     define_singleton_method(:all) do
-      @@all_tasks
+      returned_tasks = DB.exec("SELECT * FROM tasks;")
+       tasks = []
+       returned_tasks.each() do |task|
+         description = task.fetch("description")
+         tasks.push(Task.new({:description => description}))
+       end
+       tasks
     end
 #method for saving all tasks
     define_method(:save) do
-      @@all_tasks.push(self)
+      DB.exec("INSERT INTO tasks (description) VALUES ('#{@description}');")
     end
+
+    define_method(:==) do |another_task|
+     self.description().==(another_task.description())
+     end
+
+     define_method(:description) do
+       @description
+     end
+
 #the class method for clearing all saved tasks
    define_singleton_method(:clear) do
      @@all_tasks = []
    end
-  end
+end
